1e5f845b6abd5849b403faf755709e1cbd5c0aa4
web112arch <125330076+web112arch@users.noreply.github.com>
2026-01-31 22:12:21 -0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,18 @@
-const path = require('path');
+const path = require("path");
+
+const resolveSrc = (dir) => path.resolve(__dirname, "src", dir);
 
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": resolveSrc(""),
+      "@components": resolveSrc("components"),
+      "@pages": resolveSrc("pages"),
+      "@games": resolveSrc("games"),
+      "@hooks": resolveSrc("hooks"),
+      "@utils": resolveSrc("utils"),
+      "@context": resolveSrc("context"),
+      "@assets": resolveSrc("assets"),
     },
   },
 };
-
